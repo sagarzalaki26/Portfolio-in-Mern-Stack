91d17e5372cc1f8dfca5cbd6a821db0036f23119
sagarzalaki26 <sagarzalaki26@gmail.com>
2025-08-23 22:52:46 +0530

    change

--- Fronted/src/Components/Pages/Projects.jsx
+++ Fronted/src/Components/Pages/Projects.jsx
@@ -22,7 +22,7 @@ const Projects = () => {
     <section id="projects" className="bg-gray-950 py-16" data-aos="fade-up">
       <div className="max-w-6xl mx-auto p-8 text-center text-white">
         <h2 className="text-4xl font-bold mb-6 text-purple-400">My Projects</h2>
-        <p className="text-lg mb-12 opacity-80">Some of my recent work</p>
+        <p className="text-lg mb-12 opacity-80">Some of my recent works</p>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
           {projects.map((project, index) => (
